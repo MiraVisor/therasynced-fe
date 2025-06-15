4537ec86c69b5b4461528ae906ca0ffa2ec9c526
CoderMehad <mehadnadeem@gmail.com>
2025-06-15 08:50:59 +0100

    chore: added logout functionality to profile menu

---
+++ src/components/core/Dashboard/FreelancerSide/Home/index.tsx
@@ -0,0 +1,22 @@
+import { DashboardPageWrapper } from '../../DashboardPageWrapper';
+import Charts from './Charts';
+import Stats from './Stats';
+import TodayAppointments from './TodayAppointments';
+
+const FreelancerHome = () => {
+  return (
+    <DashboardPageWrapper
+      header={<h2 className="text-xl lg:text-2xl font-semibold">Platform Overview</h2>}
+    >
+      <div className="flex flex-col gap-4 lg:gap-12">
+        <Stats />
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-8">
+          <Charts />
+          <TodayAppointments />
+        </div>
+      </div>
+    </DashboardPageWrapper>
+  );
+};
+
+export default FreelancerHome;
